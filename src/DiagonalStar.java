fa33552f6125f93047b087f401739c5acd94e7a3
Kov4lnk <kov4lnk@gmail.com>
2021-01-28 17:00:44 +0200

    redesigned to display the asterisks2

--- src/DiagonalStar.java
+++ src/DiagonalStar.java
@@ -2,20 +2,22 @@ public class DiagonalStar {
     public static void printSquareStar(int number){
         if (number < 5){
             System.out.println("Invalid Value");
-        }
+        } else {
+            int row; //rows
+            int col; //cols
 
-        int row; //rows
-        int col; //cols
-
-        for (row = 0; row < number; row++){
-            for (col = 0; col < number; col++){
-                if (row == 0 || row == number - 1 || col == 0|| col == number - 1 || col == row || col == number - 1 - row){
-                    System.out.print("*");
-                }else {
-                    System.out.print(" ");
+            for (row = 0; row < number; row++){
+                for (col = 0; col < number; col++){
+                    if (row == 0 || row == number - 1 || col == 0|| col == number - 1 || col == row || col == number - 1 - row){
+                        System.out.print("*");
+                    }else {
+                        System.out.print(" ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
+
+
     }
 }
